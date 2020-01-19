42fe14ef7c128d8229a5af0346e45e8bcc0765ba
artem.baranovskyi <artem2549@gmail.com>
2020-01-19 20:04:37 +0200

    das

--- ci/userservice/tasks/test-and-build.sh
+++ ci/userservice/tasks/test-and-build.sh
@@ -2,11 +2,12 @@
 
 set -e -u
 
-#export TERM=${TERM:-dumb}
-#version=$(<latest-version/version)-SNAPSHOT
+export TERM=${TERM:-dumb}
+version=1.0-SNAPSHOT
 
 pushd git-sources
    ./gradlew clean UserService:build
 popd
 
 cp git-sources/UserService/build/libs/*.jar splitter-jars/
+echo $version > splitter-jars/snapshotVersion
